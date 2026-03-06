4ab55e73b590303b6166d48e6bb65adfaae7c552
deepan1308 <deepan123456tn@gmail.com>
2026-03-07 02:41:42 +0530

    Add Hibernate and MySQL dependencies with initial configuration(hibernate.cfg.xml)

--- hibernate demo project/Hibernate Demo/src/main/java/com/deepan/Main.java
+++ hibernate demo project/Hibernate Demo/src/main/java/com/deepan/Main.java
@@ -1,5 +1,9 @@
 package com.deepan;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -9,5 +13,11 @@ public class Main {
        a.setAid(101);
        a.setAname("deepan");
        a.setAtech("Computer science");
+
+        Configuration config = new Configuration();
+        config.configure();
+        SessionFactory factory = config.buildSessionFactory();
+        Session session = factory.openSession();
+        session.persist(a);
     }
 }
